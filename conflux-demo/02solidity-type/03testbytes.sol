fa2ba131cbcc6c08784e7a8620d1c3aef0f13e5e
1127071993@qq.com <aini2009626>
2023-02-09 16:40:40 +0800

    22create2部署合约

--- conflux-demo/02solidity-type/03testbytes.sol
+++ conflux-demo/02solidity-type/03testbytes.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.6.0;
 //bytes1 === byte  定长字节数组，也可以直接使用 byte
 contract TestBytesl {
-    // 16进制  1个byte占8位 
+    // 16进制 一个16进制占0.5字节，0x01刚好是 1个字节 占 8位，  1个byte占8位 
     bytes1 _b = 0x01;
     bytes1 _a = hex"01"; //也可以这么表示
     bytes2 _bc = "bc"; // 分别将 b c 转成16进制
